d7a296cbb77cf25c6c4b0b47f94fe62eefae4592
Gnaneswar Sai <gnaneswarsai117@gmail.com>
2025-04-02 21:08:42 +0530

    Add Proposal download feature

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { GoogleGenAI } = require("@google/genai");
 const { marked } = require("marked");
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
 require("dotenv").config();
 
 const express = require("express");
@@ -20,35 +22,66 @@ app.get("/", (req, res) => {
 });
 
 app.post("/chat", async (req, res) => {
-  const ai = new GoogleGenAI({ apiKey: process.env.MY_API_KEY });
   let { userPrompt } = req.body;
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-pro-exp-03-25",
       contents: userPrompt,
     });
     let result = marked(response.text);
-    return res.send({ message: result }).json();
+    return res.status(200).send({ message: result }).json();
   } catch (error) {
     console.log("Error:", error);
   }
 });
 
+let i = 1
+function saveFile(data) {
+  const doc = new PDFDocument();
+  const filePath = path.join(__dirname, "proposals", `proposal${i}.pdf`);
+  const writeStream = fs.createWriteStream(filePath);
+
+  doc.pipe(writeStream);
+
+  doc.fontSize(12);
+  doc.text(data, {
+    align: "left",
+    lineGap: 5,
+  });
+
+  doc.end();
+
+  writeStream.on("finish", () => {
+    console.log("PDF Created");
+    i++;
+  });
+}
+
 app.post("/chat/proposal", async (req, res) => {
   let { userPrompt } = req.body;
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-pro-exp-03-25",
       contents: userPrompt,
       config: {
-        systemInstruction: "You are now a Proposal Generator for businesses",
+        systemInstruction:
+          "Generate a structured business proposal based on the provided inputs, including Business Name, Industry, Problem Statement, Solution, Target Market, Unique Value Proposition, Revenue Model, Operational Plan, Marketing Strategy, Financial Projections, and Funding Requirements. The output should be formal, concise, and directly present the business details without any introductions, summaries, or conclusions.",
       },
     });
     let result = marked(response.text);
-    return res.send({ message: result }).json();
+    saveFile(response.text);
+    return res.status(200).send({ message: result }).json();
   } catch (error) {
     console.log("Error:", error);
   }
 });
+
+
+app.get("/download/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, "proposals", filename);
+  res.download(filePath);
+});
+
 
 module.exports = app;
